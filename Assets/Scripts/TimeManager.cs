5400bdf5ca04db738c58d90d28ac82cc16d725ec
SashaCattaneo1412 <101577464+SashaCattaneo1412@users.noreply.github.com>
2022-06-02 15:12:32 -0300

    finished time

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public Text txt_time;
+    public Text txt_timeFloored;
+    public float elapsedTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentTime = Time.time;
+        txt_time.text = Time.time.ToString();
+        txt_timeFloored.text = Mathf.Floor(currentTime).ToString();
+        elapsedTime += Time.deltaTime;
+    }
+}
